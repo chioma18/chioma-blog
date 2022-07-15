f769524ed3563dc1d1e6114d52cbb66993c838b9
Chioma Amuwa <chioma.amuwa@gmail.com>
2022-07-15 13:21:18 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'my secret key'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
