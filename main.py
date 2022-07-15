76904a737b71bcc0df5bf8ef3ff5b3bc7b3df6ea
Chioma Amuwa <chioma.amuwa@gmail.com>
2022-07-15 18:12:31 +0100

    Add gunicorn

--- main.py
+++ main.py
@@ -14,17 +14,15 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from flask_gravatar import Gravatar
 import os
-from dotenv import load_dotenv
 
 app = Flask(__name__)
-load_dotenv("/Environment Variables/.env")
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
